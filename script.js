3f726ce9f915be237b8f52a8423879ef08643285
cvinh05n-art <cvinh05n@gmail.com>
2026-04-15 16:12:34 +0700

    Update

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const actionButton = document.getElementById('actionButton');
-    
-    actionButton.addEventListener('click', function() {
-        const names = ['Tom', 'Jerry', 'Spike'];
-        alert('Thành viên nhóm:\n' + names.join('\n'));
-        console.log('Thành viên nhóm:', names);
-    });
-});
+function showmb(){
+    const mb = ["Tom","Jerry","Spike"];
+    document.getElementById("result").innerHTML = mb.join("<br>");
+}
